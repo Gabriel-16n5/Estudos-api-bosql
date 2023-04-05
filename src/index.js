172e7c1daac65ae81edc5c07a2cf6d38e96cb517
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-05 19:16:14 -0300

    primeira parte da aula

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 import express, { response } from "express";
 import cors from "cors";
 
+const receitas = [
+    {
+        id: 1,
+        nome: "pipoca",
+        ingredientes: "milho",
+        descricao: "só fritar"
+    },
+    {
+        id: 2,
+        nome: "paçoca",
+        ingredientes: "mindurim",
+        descricao: "só socar"
+    }
+]
+
 const app = express() // app do server
 app.use(cors());
 app.get("/receitas", (request, response) =>{
 
-    const receitas = [
-        {
-            id: 1,
-            nome: "pipoca",
-            ingredientes: "milho",
-            descricao: "só fritar"
-        },
-        {
-            id: 2,
-            nome: "paçoca",
-            ingredientes: "mindurim",
-            descricao: "só socar"
-        }
-    ]
-
     response.send(receitas)
+})
+
+app.get("/receitas/:id", (request, response) => {
+    const {id} = request.params
+
+    const receita = receitas.find((item) => item.id === +id)
+
+    response.send(receita)
 })
 
 app.listen(4000, () => console.log("Ta rodando")) // normalmente não é porta fixa, geralmente de 3000 a 5999
