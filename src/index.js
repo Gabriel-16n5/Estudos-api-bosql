2e82efa545a06377c059090a75a2bf459f595720
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-04 21:20:03 -0300

    segunda parte da aula

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import express, { response } from "express";
+import cors from "cors";
 
 const app = express() // app do server
+app.use(cors());
+app.get("/receitas", (request, response) =>{
 
-app.get("/home", (request, response) =>{
-    response.send("ta em casa")
+    const receitas = [
+        {
+            id: 1,
+            nome: "pipoca",
+            ingredientes: "milho",
+            descricao: "só fritar"
+        },
+        {
+            id: 2,
+            nome: "paçoca",
+            ingredientes: "mindurim",
+            descricao: "só socar"
+        }
+    ]
+
+    response.send(receitas)
 })
 
 app.listen(4000, () => console.log("Ta rodando")) // normalmente não é porta fixa, geralmente de 3000 a 5999
