8a585ae981a63b61341b734d8f3599bc1f5be954
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-14 02:31:38 -0300

    tudo atualizado, feito get, post, delete, update com async/await e pequenas validações

--- src/index.js
+++ src/index.js
@@ -1,58 +1,142 @@
 import express, { response } from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv"
 
 
-const app = express() // app do server
+const app = express() // Criação do app do server
+
+// configuração do servidor
 app.use(cors());
 app.use(express.json())
 dotenv.config()
 
-
-let db;
+// conexão com o db
 const mongoCLient = new MongoClient(process.env.DATABASE_URL)
-mongoCLient.connect()
-.then(() => db = mongoCLient.db())
-.catch((erro) => console.log(erro.message))
+    try{
+        await mongoCLient.connect()
+        console.log("conexão com db feita")
+    }catch (erro) {
+        console.log(erro.message)
+    }
+const db = mongoCLient.db();
 
-app.get("/receitas", (request, response) => {
-    db.collection("receitas").find().toArray()
-    .then(lst => response.send(lst) )
-    .catch((erro) => response.send(erro.message).status(500))
+
+// end points da aplicação
+
+app.get("/receitas", async (request, response) => {
+    try{
+       const receitas = await db.collection("receitas").find().toArray();
+        response.status(200).send(receitas)
+    } catch (erro) {
+        res.status(500).send(erro.message)
+    }
 })
 
 
-app.get("/receitas/:id", (request, response) => {
+app.get("/receitas/:id", async (request, response) => {
     const {id} = request.params
-    // exemplo simples para entender headers//
-    const {auth} = request.headers;
 
-    if(auth !== "Gabriel"){
-        return response.status(401).send("Acesso negado");
+    try{
+        const result = await db.collection("receitas").findOne({ _id: new ObjectId(id) });
+        if(!result) {
+            return response.status(404).send("Receita não encontrada")
+        }
+        response.send(result)
+    } catch (erro) {
+        response.status(500).send(erro.message)
     }
 
-
-    const receita = receitas.find((item) => item.id === +id)
-
-    if(!receita){
-        response.status(404).send("Receita não encontrada")
-    }
-
-    response.send(receita)
 })
 
-app.post("/receitas", (request, response) => {
-    const novaReceitas = 
-        {
-            nome: request.body.nome,
-            ingredientes: request.body.ingredientes,
-            descricao: request.body.descricao
-        }
+app.post("/receitas", async (request, response) => {
+    const {nome, ingredientes, descricao} = request.body;
+    const novaReceitas = {}
+    if(nome) novaReceitas.nome = nome;
+    if(ingredientes) novaReceitas.ingredientes = ingredientes;
+    if(descricao) novaReceitas.descricao = descricao;
     
-        db.collection("receitas").insertOne(novaReceitas)
-        .then(ok => response.send("Criado com sucesso").status(201))
-        .catch(erro => response.send(erro.message).status(500))
+    try{
+        const recipe = await db.collection("receitas").findOne({nome: nome})
+        if(recipe) return response.status(409).send("receita já existente")
+
+        await db.collection("receitas").insertOne(novaReceitas)
+        response.status(201).send("Receita criada com sucesso")
+    } catch (erro) {
+        response.status(500).send(erro.message)
+    }
+})
+
+app.delete("/receitas/:id", async (request, response) => {
+    const {id} = request.params;
+    try{
+       const result = await db.collection("receitas").deleteOne({_id: new ObjectId(id)});
+       if(result.deletedCount === 0) return response.status(404).send("Item não encontrado")
+        response.status(200).send("deletado")
+    }
+
+    catch (erro) {
+        response.status(500).send("deu ruim")
+    }
+})
+
+app.delete("/receitas/muitas/:filtroIngredientes", async (request, response) => {
+    const {filtroIngredientes} = request.params;
+    try{
+       const result = await db.collection("receitas").deleteMany({ ingredientes: filtroIngredientes});
+       if (result.deletedCount === 0) return response.status(404).send("Não existe nem uma receita com esse ingrediente")
+        response.status(200).send("deletado")
+    }
+
+    catch (erro) {
+        response.status(500).send("deu ruim")
+    }
+})
+
+app.put("/receitas/:id", async (request, response) => {
+    const {id} = request.params;
+    const {nome, ingredientes, descricao} = request.body;
+
+    const receitaEditada = {}
+    if(nome) receitaEditada.nome = nome;
+    if(ingredientes) receitaEditada.ingredientes = ingredientes;
+    if(descricao) receitaEditada.descricao = descricao;
+
+    try{
+        const result = await db.collection("receitas").updateOne(
+            { _id: new ObjectId(id) },
+            { $set: receitaEditada}
+        )
+        if(result.matchedCount === 0) response.status(404).send("esse item não existe")
+        response.send("receita atualizada")
+    }
+    catch (erro) {
+        response.status(500).send("deu ruim")
+    }
+})
+
+app.put("/receitas/muitas/:filtroIngredientes", async (request, response) => {
+    const {filtroIngredientes} = request.params;
+    const {nome, ingredientes, descricao} = request.body;
+
+    const receitaEditada = {}
+    if(nome) receitaEditada.nome = nome;
+    if(ingredientes) receitaEditada.ingredientes = ingredientes;
+    if(descricao) receitaEditada.descricao = descricao;
+
+    try{
+        const result = await db.collection("receitas").updateMany(
+            {ingredientes: {$regex: filtroIngredientes, $options: "i"}},
+            {$set: receitaEditada}
+        )
+
+        if(result.matchedCount === 0) return response.status(404).send("deu ruim")
+            
+            response.send("receitas editadas")
+
+    } catch (erro) {
+        response.status(500).send(erro.message)
+    }
 })
 
 app.listen(4000, () => console.log("Ta rodando")) // normalmente não é porta fixa, geralmente de 3000 a 5999
